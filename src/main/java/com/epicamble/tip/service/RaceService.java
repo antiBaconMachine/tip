8838c87d4291be68c5480b130d8530b2a209d687
Ollie Edwards <oliver.s.edwards@gmail.com>
2013-01-23 19:42:32 +0000

    service layer

---
+++ src/main/java/com/epicamble/tip/service/RaceService.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.epicamble.tip.service;
+
+import com.epicamble.tip.model.Race;
+import java.util.List;
+
+/**
+ *
+ * @author Ollie Edwards <oliver.s.edwards@gmail.com>
+ */
+public interface RaceService {
+    
+    public Race create(Race race);
+    
+    public List<Race> findAll();
+    
+    public Race findById(long id);
+    
+    public Race update(Race race); 
+    
+    public void delete(long id);
+    
+}
---
+++ src/main/java/com/epicamble/tip/service/impl/RaceServiceImpl.java
@@ -0,0 +1,47 @@
+package com.epicamble.tip.service.impl;
+
+import com.epicamble.tip.model.Race;
+import com.epicamble.tip.repository.RaceRepository;
+import com.epicamble.tip.service.RaceService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Ollie Edwards <oliver.s.edwards@gmail.com>
+ */
+@Component
+public class RaceServiceImpl implements RaceService {
+    
+    @Autowired
+    private RaceRepository raceRepository;
+
+    @Override
+    public Race create(Race race) {
+        return raceRepository.save(race);
+    }
+
+    @Override
+    public List<Race> findAll() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Race findById(long id) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Race update(Race race) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void delete(long id) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    
+
+}
